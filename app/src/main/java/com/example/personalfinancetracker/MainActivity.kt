40fe459164e3023b62c75b95efe9a405d3ca71ff
rushxdev <rashmikapabasara@gmail.com>
2025-04-14 19:37:20 +0530

    second-commit-minor

--- app/src/main/java/com/example/personalfinancetracker/MainActivity.kt
+++ app/src/main/java/com/example/personalfinancetracker/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.personalfinancetracker
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import java.util.ArrayList
 import androidx.recyclerview.widget.LinearLayoutManager
